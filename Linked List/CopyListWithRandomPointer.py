9f2b86e8a78f6bab08f82b49ee06e44fc04a67f5
Yeon Lee <82836242+yeonholee50@users.noreply.github.com>
2024-05-20 09:18:10 -0400

    finished random pointer

---
+++ Linked List/CopyListWithRandomPointer.py
@@ -0,0 +1,25 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+
+class Solution:
+    def copyRandomList(self, head: "Node") -> "Node":
+        hm = {None: None}
+        curr = head
+        while curr:
+            copy = Node(curr.val)
+            hm[curr] = copy
+            curr = curr.next
+        curr = head
+        while curr:
+            copy = hm[curr]
+            copy.next = hm[curr.next]
+            copy.random = hm[curr.random]
+            curr = curr.next
+        return hm[head]
