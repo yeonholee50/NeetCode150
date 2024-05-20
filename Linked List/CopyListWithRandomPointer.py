e8126dcf673cd302d5620847da5218fea8421ab6
Yeon Lee <82836242+yeonholee50@users.noreply.github.com>
2024-05-20 09:19:33 -0400

    made code a little more readable

--- Linked List/CopyListWithRandomPointer.py
+++ Linked List/CopyListWithRandomPointer.py
@@ -10,7 +10,8 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: "Node") -> "Node":
-        hm = {None: None}
+        hm = {}
+        hm[None] = None
         curr = head
         while curr:
             copy = Node(curr.val)
