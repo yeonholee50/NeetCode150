58e5546020c664c11a9fa90f3e3975dd35667031
Yeon Lee <yeonholee50@gmail.com>
2024-06-03 10:42:46 -0400

    just do inorder and then use a sliding window

--- Trees/isValidBST.py
+++ Trees/isValidBST.py
@@ -7,28 +7,23 @@
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
         """
-        we can pass on left and right node subtree if true. if false, we propage false value. if true is returned, compare curre node with left subtree max -> should be less than curr.val and min of right subtree -> should be greater than curr node.val
-        if so, append to our list which is left subtree lsit + right sub tree lsit + curr.val and then pass it back with a true sayign out current recursion call passed - might need to make separate function
-
+        Simplest solution is to construct a list an ensure that the left is always bigger than right subtree. or we can get a max min from each construction 
+        of arr passed back. But that'll take too long
         """
-        return self.valid_bst_traversal(root)[-1]
-        
-    def valid_bst_traversal(self, root):
-        if root == None:
-            return [], True
+        def inorder(node):
+            if node == None:
+                return []
+            else:
+                left_arr = inorder(node.left)
+                right_arr = inorder(node.right)
+                return left_arr + [node.val] + right_arr
+        arr = inorder(root)
+        if len(arr) == 1:
+            return True
         else:
-            left_arr, left_bool = self.valid_bst_traversal(root.left)
-            if left_bool == False:
-                return left_arr, False
-            right_arr, right_bool = self.valid_bst_traversal(root.right)
-            if right_bool == False:
-                return right_arr, False
-            if len(left_arr) != 0 and max(left_arr) >= root.val:
-                return [], False
-            if len(right_arr) != 0 and min(right_arr) <= root.val:
-                return [], False
-            """
-            now valid
-            """ 
-            res = left_arr + [root.val] + right_arr
-            return res, True
+            i = 0
+            j = 1
+            while j < len(arr) and arr[j] > arr[i]:
+                i+=1
+                j+=1
+            return j == len(arr)
