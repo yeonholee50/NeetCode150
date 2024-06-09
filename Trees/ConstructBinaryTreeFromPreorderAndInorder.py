7c5d01249cfab760949f8cfa43a43c8fff6251b6
Yeon Lee <yeonholee50@gmail.com>
2024-06-09 09:37:47 -0400

    modified cbtfpai

--- Trees/ConstructBinaryTreeFromPreorderAndInorder.py
+++ Trees/ConstructBinaryTreeFromPreorderAndInorder.py
@@ -6,11 +6,12 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        if not preorder or not inorder:
+        if not preorder and not inorder:
             return None
-
-        root = TreeNode(preorder[0])
-        mid = inorder.index(preorder[0])
-        root.left = self.buildTree(preorder[1:mid + 1], inorder[:mid])
-        root.right = self.buildTree(preorder[mid + 1:], inorder[mid + 1:])
-        return root
+        else:
+            root = TreeNode(preorder[0])
+            middle = inorder.index(preorder[0])
+            root.left = self.buildTree(preorder[1:middle+1], inorder[:middle])
+            root.right = self.buildTree(preorder[middle+1:], inorder[middle+1:])
+            return root
+        
