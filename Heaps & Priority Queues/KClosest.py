c557b04466ea2a00ccc69a118768ae782cfd109e
Yeon Lee <yeonholee50@gmail.com>
2024-06-17 08:07:39 -0400

    improved kclosest

--- Heaps & Priority Queues/KClosest.py
+++ Heaps & Priority Queues/KClosest.py
@@ -1,11 +1,12 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        def euclid(point):
+        if len(points) <= k:
+            return points
+        def euclid_from_center(point):
             x, y = point
             return math.sqrt(x*x + y*y)
-        arr = []
-        for point in points:
-            heapq.heappush(arr, (euclid(point), point))
+        arr = [(euclid_from_center(point), point) for point in points]
+        heapq.heapify(arr)
         res = []
         for i in range(k):
             res.append(heapq.heappop(arr)[-1])
