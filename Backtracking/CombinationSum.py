f57de9782512a56d7a58a346a9986d36f40edf95
Yeon Lee <yeonholee50@gmail.com>
2024-06-22 06:52:52 -0400

    redid combinationsum

--- Backtracking/CombinationSum.py
+++ Backtracking/CombinationSum.py
@@ -2,15 +2,16 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
 
-        def dfs(i, combination, total):
-            if total == target:
-                res.append(combination.copy())
+        def dfs(arr, i, summation):
+            if summation == target:
+                res.append(arr.copy())
                 return
-            elif i >= len(candidates) or total > target:
+            if i >= len(candidates) or summation > target:
                 return
-            combination.append(candidates[i])
-            dfs(i, combination, total + candidates[i])
-            combination.pop()
-            dfs(i+1, combination, total)
-        dfs(0, [], 0)
+            arr.append(candidates[i])
+            dfs(arr, i, summation + candidates[i])
+            arr.pop()
+            dfs(arr, i + 1, summation)
+                
+        dfs([], 0, 0)
         return res
