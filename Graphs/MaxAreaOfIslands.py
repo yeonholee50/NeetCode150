0c1f50041be0fd92539bf167c026904a2e5aec14
Yeon Lee <yeonholee50@gmail.com>
2024-06-24 12:12:25 -0400

    reduced space taek up on max def

--- Graphs/MaxAreaOfIslands.py
+++ Graphs/MaxAreaOfIslands.py
@@ -1,27 +1,25 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        if not grid or not grid[0]:
-            return 0
-        visit = set()
-        islands = 0
-        rows, cols = len(grid), len(grid[0])
+        island_locations = set()
         max_area = 0
-        def dfs(i, j, curr_islands):
+        count = 0
+        def dfs(i, j):
+            nonlocal count
             nonlocal max_area
-            if i not in range(rows) or j not in range(cols) or grid[i][j] == 0 or (i, j) in visit:
+            if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or (i, j) in island_locations or grid[i][j] == 0:
                 return
             else:
-                visit.add((i, j))
-                curr_islands.append((i, j))
-                max_area = max(max_area, len(curr_islands))
-                directions = [[-1, 0,], [1, 0], [0, -1], [0, 1]]
-                for direction in directions:
-                    dfs(direction[0] + i, direction[1] + j, curr_islands)
-
+                count+=1
+                island_locations.add((i, j))
+                dfs(i, j + 1)
+                dfs(i, j - 1)
+                dfs(i + 1, j)
+                dfs(i - 1, j)
+                max_area = max(max_area, count)
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == 1 and (i, j) not in visit:
-                    dfs(i, j, [])
-                    
+                count = 0
+                if grid[i][j] == 1 and (i, j) not in island_locations:
+                    dfs(i, j)
         return max_area
         
