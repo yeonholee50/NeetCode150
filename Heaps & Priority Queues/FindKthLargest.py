8e4fad8d83ace7187baec3655a03b11fb43c6dee
Yeon Lee <yeonholee50@gmail.com>
2024-07-01 09:43:51 -0400

    coded a more intuitive findkthlargest

--- Heaps & Priority Queues/FindKthLargest.py
+++ Heaps & Priority Queues/FindKthLargest.py
@@ -1,8 +1,9 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        max_heap = [-num for num in nums]
-        heapq.heapify(max_heap)
-        ans = None
+        arr = [-num for num in nums]
+        heapq.heapify(arr)
+        k = min(k, len(nums))
+        res = None
         for i in range(k):
-            ans = heapq.heappop(max_heap)
-        return -ans
+            res = heapq.heappop(arr)
+        return -res
