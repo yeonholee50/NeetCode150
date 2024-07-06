5979cf19dbea3d888fff4abb0e9eed57d1e2136c
Yeon Lee <yeonholee50@gmail.com>
2024-07-06 06:02:19 -0400

    accounted for better solution because k <= len(nums)

--- Heaps & Priority Queues/FindKthLargest.py
+++ Heaps & Priority Queues/FindKthLargest.py
@@ -2,8 +2,7 @@ class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         arr = [-num for num in nums]
         heapq.heapify(arr)
-        k = min(k, len(nums))
         res = None
         for i in range(k):
-            res = heapq.heappop(arr)
-        return -res
+            res = -heapq.heappop(arr)
+        return res
