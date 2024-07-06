3c195bee465e0f7fe17b0516c8abe9b177e1da25
Yeon Lee <yeonholee50@gmail.com>
2024-07-06 09:56:30 -0400

    restaged leastInterval

--- Heaps & Priority Queues/TaskScheduler.py
+++ Heaps & Priority Queues/TaskScheduler.py
@@ -1,19 +1,22 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         count = Counter(tasks)
-        max_heap = [-cnt for cnt in count.values()]
-        heapq.heapify(max_heap)
+        maxHeap = [-cnt for cnt in count.values()]
+        heapq.heapify(maxHeap)
 
         time = 0
         q = deque()
 
-        while max_heap or q:
+        while maxHeap or q:
             time+=1
-            if max_heap:
-                cnt = 1 + heapq.heappop(max_heap)
-                if cnt < 0:
-                    q.append([cnt, time + n])
+
+            if not maxHeap:
+                time = q[0][1]
+            else:
+                count = 1 + heapq.heappop(maxHeap)
+                if count:
+                    q.append((count, time + n))
             if q and q[0][1] == time:
-                cnt = q.popleft()[0]
-                heapq.heappush(max_heap, cnt)
+                heapq.heappush(maxHeap, q.popleft()[0])
         return time
+        
