66bde311403971edf70a5e78b41458e2e6e18ed2
Yeon Lee <yeonholee50@gmail.com>
2024-07-10 08:02:06 -0400

    reconfiged wordsearch

--- Backtracking/WordSearch.py
+++ Backtracking/WordSearch.py
@@ -1,37 +1,19 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        
         ROWS, COLS = len(board), len(board[0])
         path = set()
 
-        def dfs(r, c, i):
-            if i == len(word):
+        def dfs(i, r, c):
+            if i >= len(word):
                 return True
-            if (
-                r < 0 or c < 0
-                or r >= ROWS
-                or c >= COLS
-                or word[i] != board[r][c]
-                or (r, c) in path
-            ):
+            elif min(r, c) < 0 or r >= ROWS or c >= COLS or board[r][c] != word[i] or (r, c) in path:
                 return False
             path.add((r, c))
-            res = (
-                dfs(r + 1, c, i + 1)
-                or dfs(r - 1, c, i + 1)
-                or dfs(r, c + 1, i + 1)
-                or dfs(r, c - 1, i + 1)
-            )
+            exists = dfs(i + 1, r + 1, c) or dfs(i + 1, r - 1, c) or dfs(i + 1, r, c + 1) or dfs(i + 1, r, c - 1)
             path.remove((r, c))
-            return res
-
-        # To prevent TLE,reverse the word if frequency of the first letter is more than the last letter's
-        count = defaultdict(int, sum(map(Counter, board), Counter()))
-        if count[word[0]] > count[word[-1]]:
-            word = word[::-1]
-            
-        for r in range(ROWS):
-            for c in range(COLS):
-                if dfs(r, c, 0):
+            return exists
+        for i in range(len(board)):
+            for j in range(len(board[i])):
+                if dfs(0, i, j):
                     return True
         return False
