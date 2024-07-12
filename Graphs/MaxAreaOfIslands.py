64dbeb049445b5b65b2100879bb735a4dfae518c
Yeon Lee <yeonholee50@gmail.com>
2024-07-12 08:11:56 -0400

    refactored maxarea

--- Graphs/MaxAreaOfIslands.py
+++ Graphs/MaxAreaOfIslands.py
@@ -1,25 +1,29 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        island_locations = set()
+        
+        land_set = set()
         max_area = 0
-        count = 0
+        current_area = 0
+        ROWS = len(grid)
+        COLS = len(grid[0])
         def dfs(i, j):
-            nonlocal count
-            nonlocal max_area
-            if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or (i, j) in island_locations or grid[i][j] == 0:
+            nonlocal current_area
+            if min(i, j) < 0 or i >= ROWS or j >= COLS or grid[i][j] == 0 or (i, j) in land_set:
                 return
             else:
-                count+=1
-                island_locations.add((i, j))
+                land_set.add((i, j))
+                current_area+=1
                 dfs(i, j + 1)
                 dfs(i, j - 1)
                 dfs(i + 1, j)
                 dfs(i - 1, j)
-                max_area = max(max_area, count)
+
+
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                count = 0
-                if grid[i][j] == 1 and (i, j) not in island_locations:
+                if grid[i][j] == 1 and (i, j) not in land_set:
+                    current_area = 0
                     dfs(i, j)
+                    max_area = max(max_area, current_area)
         return max_area
         
