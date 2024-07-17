851c8e44afb1a8a08739f0c82d5bd80806c9548a
Yeon Lee <yeonholee50@gmail.com>
2024-07-17 10:07:36 -0400

    completed pa

--- Graphs/PacificAtlanticWaterFlow.py
+++ Graphs/PacificAtlanticWaterFlow.py
@@ -1,35 +1,28 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        ROWS, COLS = len(heights), len(heights[0])
-        pac, atl = set(), set()
+        rows, cols = len(heights), len(heights[0])
+        atl, pac = set(), set()
 
         def dfs(r, c, visit, prevHeight):
-            if (
-                (r, c) in visit
-                or r < 0
-                or c < 0
-                or r == ROWS
-                or c == COLS
-                or heights[r][c] < prevHeight
-            ):
+            if min(r, c) < 0 or r >= rows or c >= cols or (r, c) in visit or prevHeight > heights[r][c]:
                 return
             visit.add((r, c))
-            dfs(r + 1, c, visit, heights[r][c])
-            dfs(r - 1, c, visit, heights[r][c])
-            dfs(r, c + 1, visit, heights[r][c])
-            dfs(r, c - 1, visit, heights[r][c])
+            height = heights[r][c]
+            dfs(r + 1, c, visit, height)
+            dfs(r - 1, c, visit, height)
+            dfs(r, c + 1, visit, height)
+            dfs(r, c - 1, visit, height)
 
-        for c in range(COLS):
+        for c in range(cols):
             dfs(0, c, pac, heights[0][c])
-            dfs(ROWS - 1, c, atl, heights[ROWS - 1][c])
-
-        for r in range(ROWS):
+            dfs(rows - 1, c, atl, heights[rows-1][c])
+        for r in range(rows):
             dfs(r, 0, pac, heights[r][0])
-            dfs(r, COLS - 1, atl, heights[r][COLS - 1])
+            dfs(r, cols - 1, atl, heights[r][cols - 1])
 
         res = []
-        for r in range(ROWS):
-            for c in range(COLS):
+        for r in range(rows):
+            for c in range(cols):
                 if (r, c) in pac and (r, c) in atl:
                     res.append([r, c])
         return res
