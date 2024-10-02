a4f4b3288ca23aae4cf092d44aab8e37a7ab9070
Yeon Lee <yeonholee50@gmail.com>
2024-10-02 07:06:08 -0400

    fully coded jump game

--- Greedy Algorithms/JumpGame.py
+++ Greedy Algorithms/JumpGame.py
@@ -1,9 +1,9 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         
+        
         goal = len(nums) - 1
-
-        for i in range(len(nums) - 2, -1, -1):
-            if i + nums[i] >= goal:
+        for i in range(len(nums) - 1, -1, -1):
+            if nums[i] + i >= goal:
                 goal = i
         return goal == 0
