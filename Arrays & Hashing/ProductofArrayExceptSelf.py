fae194a20fd3cab78a05da1a822b5674770f235f
Yeon Lee <yeonholee50@gmail.com>
2025-01-27 15:43:32 -0500

    finished 1_27_2025_neetcode

--- Arrays & Hashing/ProductofArrayExceptSelf.py
+++ Arrays & Hashing/ProductofArrayExceptSelf.py
@@ -1,26 +1,20 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        """
-        if there's two 0s, than we return array of 0s
-        if there's 1 0, than we put the product in that index and the rest is 0s
-        """
-        prod = 1
-        res = [0] * len(nums)
-        zero = False
-        index = None
+        zero_index = None
+
+        product = 1
         for i in range(len(nums)):
             num = nums[i]
-            if num == 0 and zero == True:
-                return res
-            elif num == 0:
-                zero = True
-                index = i
+            if num == 0 and zero_index is not None:
+                return [0] * len(nums)
+            elif num == 0 and zero_index is None:
+                zero_index = i
             else:
-                prod*=num
-        if zero == True:
-            res[index] = prod
-            return res
-
-        for i in range(len(res)):
-            res[i] = prod // nums[i]
-        return res
+                product*=num
+        if zero_index is not None:
+            product_array = [0] * len(nums)
+            product_array[zero_index] = product
+            return product_array
+        else:
+            product_array = [int(product//num) for num in nums]
+            return product_array
