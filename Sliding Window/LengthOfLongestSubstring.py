eaec2a9b845d22edb0e5fb598af2fd6935a55e9d
Yeon Lee <yeonholee50@gmail.com>
2025-01-31 12:56:39 -0500

    completed neetcode for 1_31_2025

--- Sliding Window/LengthOfLongestSubstring.py
+++ Sliding Window/LengthOfLongestSubstring.py
@@ -1,14 +1,25 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        if len(s) <= 1:
+            return len(s)
+        letters = set()
         
-        charSet = set()
-        l = 0
-        res = 0
-
-        for r in range(len(s)):
-            while s[r] in charSet:
-                charSet.remove(s[l])
-                l += 1
-            charSet.add(s[r])
-            res = max(res, r - l + 1)
-        return res
+        l, r = 0, 1
+        letters.add(s[l])
+        maximum = 1
+        while r < len(s):
+            if s[r] in letters:
+                
+                l+=1
+                letters = set()
+                letters.add(s[l])
+                r = l + 1
+            else:
+                letters.add(s[r])
+                r+=1
+                
+        
+            maximum = max(maximum, r - l)
+        
+        
+        return maximum
