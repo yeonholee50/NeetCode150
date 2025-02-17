f2f5cd95e7954583b18474531eda7702d5b7c044
Yeon Lee <yeonholee50@gmail.com>
2025-02-17 07:00:40 -0500

    completed lru and dup finder

--- Linked List/FindtheDuplicatedNumber.py
+++ Linked List/FindtheDuplicatedNumber.py
@@ -1,6 +1,7 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         slow, fast = 0, 0
+
         while True:
             slow = nums[slow]
             fast = nums[nums[fast]]
@@ -8,8 +9,8 @@ class Solution:
                 break
 
         slow2 = 0
-        while True:
+
+        while slow != slow2:
             slow = nums[slow]
             slow2 = nums[slow2]
-            if slow == slow2:
-                return slow
+        return slow
--- Linked List/LRUCache.py
+++ Linked List/LRUCache.py
@@ -1,33 +1,32 @@
 class Node:
-    def __init__(self, key, val):
-        self.key, self.val = key, val
+    def __init__(self, key, value):
+        self.key, self.value = key, value
         self.prev = self.next = None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cap = capacity
-        self.cache = {} #will map key to node
+        self.capacity = capacity
+        self.cache = {}
         self.left, self.right = Node(0, 0), Node(0, 0)
         self.left.next, self.right.prev = self.right, self.left
 
-    # remove node from list
     def remove(self, node):
         prev, nxt = node.prev, node.next
         prev.next, nxt.prev = nxt, prev
 
-    # insert node at right
     def insert(self, node):
         prev, nxt = self.right.prev, self.right
         prev.next = nxt.prev = node
-        node.next, node.prev = nxt, prev
+        node.prev, node.next = prev, nxt
 
     def get(self, key: int) -> int:
         if key in self.cache:
             self.remove(self.cache[key])
             self.insert(self.cache[key])
-            return self.cache[key].val
+            return self.cache[key].value
         return -1
+    
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
@@ -35,8 +34,14 @@ class LRUCache:
         self.cache[key] = Node(key, value)
         self.insert(self.cache[key])
 
-        if len(self.cache) > self.cap:
-            # remove from the list and delete the LRU from hashmap
+
+        if len(self.cache) > self.capacity:
             lru = self.left.next
             self.remove(lru)
             del self.cache[lru.key]
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
